d77fa783a29f7d01301fb291d4ac9ed553656fbf
AeroBotX <p.garg1606@gmail.com>
2025-12-24 17:24:06 -0500

    updated API URL

--- country-api/script.js
+++ country-api/script.js
@@ -20,7 +20,7 @@ function getURLParameter(name) {
 }
 
 const selectedCountryName = getURLParameter('country');
-const API_URL = 'http://129.80.16.185:8000/countries';
+const API_URL = 'https://ambient-equally-css-completed.trycloudflare.com/countries';
 
 async function renderCountryData() {
     const mainContainer = document.getElementById('main-content');
